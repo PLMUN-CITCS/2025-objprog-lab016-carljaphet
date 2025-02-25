3eeb4cb69ee663ed2641545bbf550f392176fef5
carljaphet <alorocarljaphet_bscs@plmun.edu.ph>
2025-02-25 15:44:55 +0800

    Submitting OBJPROG WEEK 05 - LABORATORY #16

---
+++ MethodOverloading.java
@@ -0,0 +1,17 @@
+
+public class MethodOverloading{
+    public static void main(String[] args){
+      printValue(10);
+      printValue(3.14);
+      printValue("hello!");  
+    }
+    public static void printValue(int number){
+        System.out.println("integer value: " + number);
+    }
+    public static void printValue(double number) {
+        System.out.println("double value: " + number);
+    }
+    public static void printValue(String text){
+        System.out.println("string value: " + text);
+    }
+}
